894b45afe529065f5ce936e43ffb0986842d97f5
Dimitrios Barkas <dimitriosbarkas@Dimitrioss-MBP.fritz.box>
2021-07-01 18:59:44 +0200

    trigger the gcr

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hier entsteht Libera.
+          Hier entsteht Libera. Die Webapplikation
         </p>
         <a
           className="App-link"

0265a162efb6b45db2a08ff3f20a5808bff89d4e
Dimitrios Barkas <dimitriosbarkas@Dimitrioss-MBP.fritz.box>
2021-07-05 16:37:20 +0200

    allround project improved
    backend: articles crud operations implemented
    
    frontend: added login window

--- backend/src/api/articles/articles.controller.js
+++ backend/src/api/articles/articles.controller.js
@@ -1,4 +1,5 @@
 import ArticlesDAO from "../../dao/ArticlesDAO.js"
+import { constants } from "http2"
 
 export default class ArticlesController {
   static async apiGetArticles(req, res, next) {
@@ -57,5 +58,160 @@ export default class ArticlesController {
     res.json(response)
   }
 
-  static async apiAddArticle(req, res, next) {}
+  static async apiInsertArticle(req, res, next) {
+    try {
+      const body = req.body
+      let errors = {}
+      if (body && body.name.length < 3) {
+        errors.name = "The name of the article must be at least 3 characters."
+      }
+
+      if (Object.keys(errors).length > 0) {
+        res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
+          code: "INSERTION_FAILED",
+          message: errors.name,
+        })
+        return
+      }
+
+      const response = await ArticlesDAO.insertArticle(body.name)
+      if (!response) {
+        res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
+          code: "ARTICLE_ALREADY_EXISTS",
+          message: `Insertion failed, article with name ${body.name} already exist`,
+        })
+        return
+      }
+      res.status(constants.HTTP_STATUS_OK).json({
+        code: "INSERTION_SUCCED",
+        message: `${response.insertedCount} article with name ${body.name} and id ${response.insertedId} inserted.`,
+      })
+    } catch (error) {
+      res
+        .status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
+        .json({ error: error })
+    }
+  }
+
+  static async apiGetArticleById(req, res, next) {
+    try {
+      let id = req.params.id || {}
+      let article = await ArticlesDAO.getArticleById(id)
+      if (!article) {
+        res.status(constants.HTTP_STATUS_NOT_FOUND).json({
+          code: "ARTICLE_NOT_FOUND",
+          message: `Article with id ${id} could not be found.`,
+        })
+        return
+      }
+
+      if (article.error) {
+        res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
+          code: "ID_INCORRECT",
+          message: `Entered id ${id} was incorrect`,
+        })
+        return
+      }
+
+      res.status(constants.HTTP_STATUS_OK).json({
+        _id: article._id,
+        name: article.name,
+      })
+    } catch (error) {
+      res
+        .status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
+        .json({ error: error })
+    }
+  }
+
+  static async apiUpdateArticleById(req, res, next) {
+    try {
+      let id = req.params.id || {}
+      let body = req.body
+      let errors = {}
+
+      if (Object.keys(body).length === 0) {
+        errors.name =
+          "You have to specify a new or modified name for the article inside the body of your request."
+      } else if (body && body.name?.length < 3) {
+        errors.name = "The name of the article must be at least 3 characters."
+      }
+      if (id?.length !== 24) {
+        errors.id = "An error occured with the id"
+      }
+
+      if (Object.keys(errors).length > 0) {
+        res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
+          code: "PATCH_FAILED",
+          message: errors,
+        })
+        return
+      }
+
+      const {
+        matchedCount,
+        modifiedCount,
+      } = await ArticlesDAO.updateArticleById(id, body.name)
+
+      if (matchedCount === 1 && modifiedCount === 0) {
+        res.status(constants.HTTP_STATUS_NOT_MODIFIED).json({
+          code: "ARTICLE_NOT_MODIFIED",
+          message: "Article was found but not modified.",
+        })
+        return
+      }
+
+      if (!modifiedCount) {
+        res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
+          code: "PATCH_FAILED",
+          message: `No article with id ${id} found.`,
+        })
+        return
+      }
+
+      res.status(constants.HTTP_STATUS_OK).json({
+        code: "PATCH_SUCCED",
+        message: `${modifiedCount} Object with id ${id} modified.`,
+      })
+    } catch (error) {
+      res
+        .status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
+        .json({ error: error })
+    }
+  }
+
+  static async apiDeleteArticleById(req, res, next) {
+    try {
+      let id = req.params.id || {}
+      let errors = {}
+      if (id.length !== 24) {
+        errors.id = "An error occured with the id"
+      }
+      if (Object.keys(errors).length > 0) {
+        res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
+          code: "DELETION_FAILED",
+          message: errors,
+        })
+        return
+      }
+      const response = await ArticlesDAO.deleteArticleById(id)
+
+      if (response.deletedCount < 1) {
+        res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
+          code: "DELETION_FAILED",
+          message: `No Article with id ${id} found.`,
+        })
+        return
+      }
+
+      res.status(constants.HTTP_STATUS_OK).json({
+        code: "DELETION_SUCCED",
+        message: `${response.deletedCount} Article with id ${id} deleted.`,
+      })
+    } catch (error) {
+      res
+        .status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
+        .json({ error: error })
+    }
+  }
 }
--- backend/src/api/articles/articles.route.js
+++ backend/src/api/articles/articles.route.js
@@ -5,5 +5,9 @@ const router = new Router()
 
 router.route("/").get(ArticlesController.apiGetArticles)
 router.route("/search").get(ArticlesController.apiSearchArticles)
+router.route("/").post(ArticlesController.apiInsertArticle)
+router.route("/id/:id").get(ArticlesController.apiGetArticleById)
+router.route("/id/:id").patch(ArticlesController.apiUpdateArticleById)
+router.route("/id/:id").delete(ArticlesController.apiDeleteArticleById)
 
 export default router
